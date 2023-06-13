4102fa5a4ccc9e77430c295f0f6398e0e5b4ce74
nob-swik <nobusuke.sawaike@denso-ten.com>
2023-06-14 07:57:36 +0900

    HTML display ver

--- web.go
+++ web.go
@@ -3,8 +3,13 @@ package main
 import "net/http"
 
 func main() {
+	msg :=`<html><body>
+	<h1>Hello</h1>
+	<p>This is GO-server!!</p>
+	</body></html>`
+
 	hh := func(w http.ResponseWriter, rq *http.Request) {
-		w.Write([]byte("Hello, This is GO-server!!"))
+		w.Write([]byte(msg))
 	}
 
 	http.HandleFunc("/hello", hh)
